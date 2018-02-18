04b4a57c38aa64405f031f8d737ff6d043bbf6db
Justin M <mckennaman123@users.noreply.github.com>
2018-02-18 09:38:59 -0500

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,35 @@
+//==============================================================================
+// Basics for openning a file for milestone 2 of Project 1.
+//
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include "bigint.hpp"
+
+int main() {
+    std::ifstream in("data1-1.txt");    // Define stream for input
+    if(!in) {                           // Make sure it opened correctly.
+        std::cerr << "Could not open data1-1.txt, exiting." << std::endl;
+        exit(1);
+    }
+
+    std::cout << "Gets bigints from file and adds them until end of file:"
+	      << std::endl << std::endl;
+
+    bigint lhs, rhs, result;
+    while(!in.eof() && in >> lhs) {
+	//getting bigints from input
+	if(!in.eof()) in >> rhs;
+
+	//prints bigints
+	std::cout << "lhs = " << lhs << std::endl << std::endl
+		  << "rhs = " << rhs << std::endl << std::endl;
+
+	//adds bigints and print result
+	result = lhs + rhs;
+	std::cout << "result = " << result << std::endl << std::endl;
+    }
+
+    in.close();
+    return 0;
+}
